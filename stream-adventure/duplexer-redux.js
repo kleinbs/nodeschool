cd9fe48ae81777ee6493c7b28a96de511261385a
Klein, Brian (HAL) <BKlein@HollandAmerica.com>
2015-04-20 12:12:22 -0700

    This one was hard, It took a lot of help

--- stream-adventure/duplexer-redux.js
+++ stream-adventure/duplexer-redux.js
@@ -1,23 +1,22 @@
-var duplex = require('duplexer2');
+var duplexer = require('duplexer2');
 var through = require('through2').obj;
 
 module.exports = function (counter) {
 
-	var countryCount = {}
+	var countryCount = {};
 	var writeable = through(write, end);
 
-	return duplex(writeable, countryCount);
+	return duplexer(writeable, counter);
 
-	function write(buf, enc, next){
-		if(countryCount[buf.country] != undefined){
-			countryCount[buf.country] = 0
+	function write (buf, enc, next){
+		if(countryCount[buf.country] == undefined){
+			countryCount[buf.country] = 1
 		} else {
 			countryCount[buf.country] ++;
 		}
 		next();
 	}
-
-	function end(done){
+	function end (done){
 		counter.setCounts(countryCount);
 		done();
 	}
