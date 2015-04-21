8422d4edd1bb762747dcd499c45452149c9a9d39
Klein, Brian (HAL) <BKlein@HollandAmerica.com>
2015-04-21 14:37:49 -0700

    starting last project

---
+++ stream-adventure/secretz.js
@@ -0,0 +1,12 @@
+var tar = require('tar');
+var crypto = require('crypto');
+var zlib = require('zlib');
+var parser = tar.Parse();
+
+var decrypt = crypto.createDecipher(process.argv[2], process.argv[3]);
+
+parser.on('entry', function (e) {
+    return e;
+});
+
+process.stdin.pipe(decrypt).pipe(zlib.createGunzip()).pipe(process.stdout)
