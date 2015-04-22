6eba4ef44a5b05364d1e04022774823cd6d37b30
kleinbs <kleinbs1234@gmail.com>
2015-04-21 21:18:03 -0700

    Got it! Done with Secretz
    DONE WITH THE LAST STREAM-ADVENTURES LESSON

--- stream-adventure/secretz.js
+++ stream-adventure/secretz.js
@@ -1,15 +1,30 @@
 var tar = require('tar');
 var crypto = require('crypto');
 var zlib = require('zlib');
+var through = require('through2')
 var parser = tar.Parse();
 
 var decrypt = crypto.createDecipher(process.argv[2], process.argv[3]);
 
-parser.on('entry', function (e) {
+parser.on('entry', function (entry) {
 
+	if(entry.type !== 'File'){
+		//console.log('hello')
+		return
+	}
+
+	var path = " " + entry.path + '\n';
+	var addHash = crypto.createHash('md5', { encoding: 'hex' });
+	//console.log(addHash)
+	var addPath = through(function(data, enc, next){
+		this.push(data + path);
+		next();
+	})
 	
-	crypto.createHash
-    return e;
+
+
+	entry.pipe(addHash).pipe(addPath).pipe(process.stdout)//.pipe(+ " " + '\n')//.pipe(process.stdout);
+
 });
 
-process.stdin.pipe(decrypt).pipe(zlib.createGunzip()).pipe(process.stdout)
+process.stdin.pipe(decrypt).pipe(zlib.createGunzip()).pipe(parser)
