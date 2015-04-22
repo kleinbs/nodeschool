fb494bfda7131f22a9966f1ab490df13db82aa5c
kleinbs <kleinbs1234@gmail.com>
2015-04-21 21:23:38 -0700

    Cleanup
    Cleaned up the code

--- stream-adventure/secretz.js
+++ stream-adventure/secretz.js
@@ -9,22 +9,17 @@ var decrypt = crypto.createDecipher(process.argv[2], process.argv[3]);
 parser.on('entry', function (entry) {
 
 	if(entry.type !== 'File'){
-		//console.log('hello')
 		return
 	}
 
 	var path = " " + entry.path + '\n';
 	var addHash = crypto.createHash('md5', { encoding: 'hex' });
-	//console.log(addHash)
 	var addPath = through(function(data, enc, next){
 		this.push(data + path);
 		next();
 	})
-	
 
-
-	entry.pipe(addHash).pipe(addPath).pipe(process.stdout)//.pipe(+ " " + '\n')//.pipe(process.stdout);
-
+	entry.pipe(addHash).pipe(addPath).pipe(process.stdout)
 });
 
 process.stdin.pipe(decrypt).pipe(zlib.createGunzip()).pipe(parser)
