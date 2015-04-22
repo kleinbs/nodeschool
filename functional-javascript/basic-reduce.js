e08350eccf674f18f40c56339ef85f3a1572cd1b
Klein, Brian (HAL) <BKlein@HollandAmerica.com>
2015-04-22 13:08:31 -0700

    Added basic reduce

--- functional-javascript/basic-reduce.js
+++ functional-javascript/basic-reduce.js
@@ -1,5 +1,13 @@
 function countWords(inputWords) {
-  return
+
+	return inputWords.reduce(function(prev, curr){
+			if(prev[curr]){
+				prev[curr] += 1;
+			} else {
+				prev[curr] = 1;
+			}
+			return prev;
+		}, {});
 }
 
 module.exports = countWords
